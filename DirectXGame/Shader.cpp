92c20373330d0881e56b424199b8e27310e8efb0
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-05-21 11:41:55 +0900

    終了

---
+++ DirectXGame/Shader.cpp
@@ -0,0 +1,46 @@
+#include "Shader.h"
+#include <cassert>
+#include <d3dcompiler.h>
+
+void Shader::Load(const std::wstring& filePath, const std::string& shaderModel) {
+
+	ID3DBlob* shaderBlob = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+
+	HRESULT hr = D3DCompileFromFile(
+
+	    filePath.c_str(), // シェーダファイル名
+	    nullptr,
+	    D3D_COMPILE_STANDARD_FILE_INCLUDE,               // インクルード可能にする
+	    "main", shaderModel.c_str(),                     // エントリーポイント名、シェーダモデル指定
+	    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
+	    0, &shaderBlob, &errorBlob);
+
+	// エラーが発生
+	if (FAILED(hr)) {
+
+		if (errorBlob) {
+
+			OutputDebugStringA(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+			errorBlob->Release();
+		}
+
+		assert(false);
+	}
+
+	// 生成したshaderBlobを取っておく
+	blob_ = shaderBlob;
+}
+
+ID3DBlob* Shader::GetBlob() { return blob_; }
+
+Shader::Shader() {}
+
+Shader::~Shader() {
+
+	if (blob_ != nullptr) {
+
+		blob_->Release();
+		blob_ = nullptr;
+	}
+}
