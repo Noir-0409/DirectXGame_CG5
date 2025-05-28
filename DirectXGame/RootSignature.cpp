3802cc6ff39b0ca9b2ea32c0c23ad58d3b711c9e
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-05-28 10:34:46 +0900

    14枚目まで

---
+++ DirectXGame/RootSignature.cpp
@@ -0,0 +1,60 @@
+#include "RootSignature.h"
+#include "KamataEngine.h"
+
+using namespace KamataEngine;
+
+void RootSignature::Create() {
+
+	//既にインスタンスがあれば解放
+	if (rootSignature_) {
+	
+	rootSignature_->Release();
+	rootSignature_ = nullptr;
+	
+	}
+
+	//クラス内で取得するために追加
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+		// RootSignature作成
+	// 構造体にデータを送る
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlog = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
+
+	if (FAILED(hr)) {
+
+		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
+
+		assert(false);
+	}
+
+	// バイナリをもとに生成
+	ID3D12RootSignature* rootSignature = nullptr;
+	hr = dxCommon->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+	assert(SUCCEEDED(hr));
+
+	//解放
+	signatureBlob->Release();
+
+	//生成したRootSignatureを取っておく
+	rootSignature_ = rootSignature;
+
+}
+
+ID3D12RootSignature* RootSignature::Get() { return rootSignature_; }
+
+RootSignature::RootSignature() {}
+
+RootSignature::~RootSignature() {
+
+	if (rootSignature_) {
+	
+	rootSignature_->Release();
+		rootSignature_ = nullptr;
+	
+	}
+
+}
