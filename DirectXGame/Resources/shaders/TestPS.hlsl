e41cc9253f8e41be5ab35f4adccab07cc6bc06a1
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-06-18 12:51:17 +0900

    uv座標変更

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -12,7 +12,7 @@ PixelShaderOutput main(VertexShaderOutput input)
     
     PixelShaderOutput output;
     float32_t2 uv = input.texcoord;
-    output.color = float32_t4(uv.x, uv.y, 1.0f, 1.0f);
+    output.color = float32_t4(uv.x, uv.y, 0.0f, 1.0f);
     return output;
     
 }
